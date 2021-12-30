7e42541ae7e6d3d2d2b804a052d4e99dc2088844
Rohit Sharma <rohit108180@gmail.com>
2021-12-30 17:36:08 +0530

    add routes , login api , register a user api

--- routes/contact.js
+++ routes/contact.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 
 router.get('/', (req,res)=>{
-    res.send('Get all contacts');
+    res.send('Get all users contacts');
 });
 
 
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.send({msg: 'Welcome to the the conatact keeper api'}));
+
+
+// Connect dataBase
+connectDB(); 
+
+// Init Middleware
+
+app.use(express.json({extended : false})); 
+
+
+app.get('/', (req, res) => res.send({msg: 'Welcome to the the conatact keeper API...'}));
 
 
 
