f2d9ebcb2b39c83e5917ec4d78616e2343255f33
Rohit Sharma <rohit108180@gmail.com>
2021-12-31 18:55:15 +0530

    add get all users contact api

--- routes/contact.js
+++ routes/contact.js
@@ -1,13 +1,26 @@
 const express =require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const User = require("../models/User");
+const Contact = require("../models/Contact");
+const auth = require('../middleware/auth');
+
 
 //@route  GET api/contacts
 // @desc  get all users Contact
 // @acces Private
 
 
-router.get('/', (req,res)=>{
-    res.send('Get all users contacts');
+router.get('/',auth , async (req,res)=>{
+    try {
+        
+        const contacts = await Contact.find({user : req.user.id}).sort({date: -1});
+        res.json(contacts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("server error");
+        
+    }
 });
 
 
